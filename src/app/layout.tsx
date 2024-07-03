ba7a1c40120ebd0f50fa69736c4e2a44d89d89ed
sanjayguwaju <sanjayguwaju@gmail.com>
2024-07-03 22:17:28 +0545

    chore: Update layout.tsx and tailwind.config.cjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
-
-import "./global.css"
+import "./global.css";
 
 export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
